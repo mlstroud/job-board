9839d87ee8dc0dc896f80e6ecec3c87f9cb5fbe4
Matt Stroud <lixingke06@gmail.com>
2020-05-26 10:46:01 -0700

    update constructor to store title

--- JobBoard/Models/JobOpening.cs
+++ JobBoard/Models/JobOpening.cs
@@ -2,6 +2,14 @@ namespace JobBoard.Models
 {
   public class JobOpening
   {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Contact { get; set; }
+    public int Id { get; set; }
 
+    public JobOpening(string title, string description, string contact)
+    {
+      Title = title;
+    }
   }
 }
